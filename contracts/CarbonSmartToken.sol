4023fc7a6d688e2422db63013fd532781efd40d0
cristophervenegasn-sketch <cristopher.venegasn@unmsm.edu.pe>
2026-07-08 04:26:36 -0500

    refactor: Update CarbonSmartToken to Solidity 0.8.28 with admin management

--- contracts/CarbonSmartToken.sol
+++ contracts/CarbonSmartToken.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./BaseRelayRecipient.sol";
 
 contract CarbonSmartToken is ERC20, BaseRelayRecipient {
     address public admin;
+
+    event AdminChanged(address indexed oldAdmin, address indexed newAdmin);
 
     constructor(address _trustedForwarder) ERC20("Green Carbon Token", "GCT") {
         admin = msg.sender;
@@ -16,6 +18,14 @@ contract CarbonSmartToken is ERC20, BaseRelayRecipient {
     function mint(address to, uint256 amount) external {
         require(msg.sender == admin, "Only admin can mint");
         _mint(to, amount);
+    }
+
+    function setAdmin(address newAdmin) external {
+        require(msg.sender == admin, "Only admin can change admin");
+        require(newAdmin != address(0), "Invalid admin address");
+        address oldAdmin = admin;
+        admin = newAdmin;
+        emit AdminChanged(oldAdmin, newAdmin);
     }
 
     function _msgSender() internal view override returns (address sender) {
